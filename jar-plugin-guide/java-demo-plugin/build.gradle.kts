e0c161ed699604a3966325e93450bcb519d83dfa
easepan <easepan@qq.com>
2024-03-13 18:22:38 +0800

    feat: update dependency version

--- jar-plugin-guide/java-demo-plugin/build.gradle.kts
+++ jar-plugin-guide/java-demo-plugin/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -18,9 +21,11 @@ repositories {
 
 val myflowVersion = "1.0.6"
 
+var myflowDependency: Dependency? = null
+var jetbrainsComposeDependency: Dependency? = null
 dependencies {
-    compileOnly(compose.desktop.currentOs)
-    compileOnly("top.myrest:myflow-kit:$myflowVersion")
+    jetbrainsComposeDependency = implementation(compose.desktop.currentOs)
+    myflowDependency = implementation("top.myrest:myflow-kit:$myflowVersion")
     testImplementation("top.myrest:myflow-baseimpl:$myflowVersion")
 }
 
@@ -36,15 +41,20 @@ tasks.jar {
     archiveFileName.set(entry)
     val exists = mutableSetOf<String>()
     val files = mutableListOf<Any>()
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     configurations.runtimeClasspath.get().allDependencies.forEach { dependency ->
+        if (dependency == myflowDependency || dependency == jetbrainsComposeDependency) {
+            return@forEach
+        }
+        println(dependency)
         configurations.runtimeClasspath.get().files(dependency).forEach { file ->
             if (exists.add(file.name)) {
+                println(file.name)
                 files.add(if (file.isDirectory) file else zipTree(file))
             }
         }
     }
     from(files)
-    exclude(".gitkeep")
 }
 
 tasks.build {
