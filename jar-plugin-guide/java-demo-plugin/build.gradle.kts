040827ade4115ef68dcb042dc14bc5874d29d0b6
easepan <easepan@qq.com>
2024-03-14 12:00:07 +0800

    feat: update version

--- jar-plugin-guide/java-demo-plugin/build.gradle.kts
+++ jar-plugin-guide/java-demo-plugin/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-val myflowVersion = "1.0.6"
+val myflowVersion = "1.0.7"
 
 var myflowDependency: Dependency? = null
 var jetbrainsComposeDependency: Dependency? = null
